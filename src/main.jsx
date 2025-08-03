dfa895bdbe5e8219874642d845deb1d9ce088bd4
Mohon Saha <practicewithmohon@gmail.com>
2025-08-03 19:18:42 +0600

    setup redux store and structure folders

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/routes'
+import { Provider } from 'react-redux'
+import { store } from './redux/features/hooks'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
   </StrictMode>,
 )
