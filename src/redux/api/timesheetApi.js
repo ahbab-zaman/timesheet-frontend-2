df29a2360087504dd6ba0be5cc0281d763cf949c
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-08-20 17:57:24 +0600

    fixed the login issue

--- src/redux/api/timesheetApi.js
+++ src/redux/api/timesheetApi.js
@@ -1,17 +1,13 @@
-
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
-
+import axiosInstance from "@/services/axiosInstance";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const timesheetApi = createApi({
-    reducerPath: 'timesheetApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:4001/api/v1/', credentials: 'include'}),
-    endpoints: () => ({}),
-
-})
-
+  reducerPath: "timesheetApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_API_BASE_URL}/api/v1/`,
+    credentials: "include",
+  }),
+  endpoints: () => ({}),
+});
 
 export default timesheetApi;
-
-
-
